27c472d0f9874e5aea29ca55ec1c1f12b31b4062
James Kevin Lin <lin.james0825@yahoo.com>
2021-02-02 14:55:51 +0800

    Added pivot table in the sql scripts

--- OLAP_Scripts_Revised.sql
+++ OLAP_Scripts_Revised.sql
@@ -50,3 +50,19 @@ where (events="clear" or events="cloudy") and
 (hour>=6 and hour<=18 )
 group by year,month, station_name  with rollup
 ;
+
+-- pivot table
+SELECT 
+if(grouping(year),"total duration",year) as year, 
+if(grouping(month), "yearly total",month) as month,
+round(sum(if (events="cloudy", trip_duration,0)),2) as "cloudy days",
+round(sum(if (events="rain or snow", trip_duration,0)),2) as "rain or snow days",
+round(sum(if (events="clear", trip_duration,0)),2) as "clear sky days",
+round(sum(if (events="tstorms", trip_duration,0)),2) as "thunder storms",
+round(sum(if (events="not clear", trip_duration,0)),2) as "unclear days",
+round(sum(if (events="unknown", trip_duration,0)),2) as "unknown weather"
+FROM trip_infos ti 
+join trip_facts tf 
+on ti.trip_id=tf.trip_id
+group by year, month with rollup
+;
